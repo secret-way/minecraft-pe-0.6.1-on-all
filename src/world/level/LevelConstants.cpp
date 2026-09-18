08aa1a631962e1c975fa488a8638c327891f465d
secret-way <m7301099@gmail.com>
2026-09-18 15:00:47 +0300

    Increase chunk dimensions in LevelConstants

--- src/world/level/LevelConstants.cpp
+++ src/world/level/LevelConstants.cpp
@@ -1,8 +1,8 @@
 #include "LevelConstants.h"
 
 int LevelConstants::LEVEL_HEIGHT = 128;
-int LevelConstants::CHUNK_CACHE_WIDTH = 16; // in chunks
-int LevelConstants::CHUNK_WIDTH = 16; // in blocks
+int LevelConstants::CHUNK_CACHE_WIDTH = 1024; // in chunks
+int LevelConstants::CHUNK_WIDTH = 1024; // in blocks
 int LevelConstants::CHUNK_DEPTH = 16;
 int LevelConstants::LEVEL_WIDTH = LevelConstants::CHUNK_CACHE_WIDTH * CHUNK_WIDTH;
 int LevelConstants::LEVEL_DEPTH = LevelConstants::CHUNK_CACHE_WIDTH * CHUNK_DEPTH;
